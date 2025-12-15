7d7d048ed6696a55e3a3e9107b060e72cfaeca80
지은 <107034832+6868x88@users.noreply.github.com>
2025-12-15 11:16:30 +0900

    Update NewsSummaryService.java

--- newsapi/src/main/java/com/example/newsapi/service/NewsSummaryService.java
+++ newsapi/src/main/java/com/example/newsapi/service/NewsSummaryService.java
@@ -1,6 +1,9 @@
 package com.example.newsapi.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.newsapi.dto.PythonSummaryResponseDto;
@@ -17,7 +20,19 @@ public class NewsSummaryService {
 
     public PythonSummaryResponseDto getNewsSummaries(int limit) {
         String url = "http://127.0.0.1:8000/news/summary?limit=" + limit;
-        return restTemplate.getForObject(url, PythonSummaryResponseDto.class);
+        try {
+            return restTemplate.getForObject(url, PythonSummaryResponseDto.class);
+        } catch (RestClientException e) {
+        	//파이썬 서버 장애일 경우
+            return fallbackResponse();
+        }
+    }
+    
+    private PythonSummaryResponseDto fallbackResponse() {
+        PythonSummaryResponseDto response = new PythonSummaryResponseDto();
+        response.setCount(0);
+        response.setArticles(List.of());
+        return response;
     }
 
 }
